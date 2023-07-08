06e77f7e9bfaaded50e7e43cebeb5f6116ff8d1b
JOGINDER571 <snshrma7@gmail.com>
2023-07-08 11:01:40 +0530

    In the react-table we can use in built (sorting,filtering,pagination,row selecting,sticky columns) many more features

---
+++ src/components/BasicTable.js
@@ -0,0 +1,55 @@
+import React, { useMemo } from "react";
+import { COLUMNS, HEADER_GROUPS } from "./columns";
+import { useTable, useSortBy } from "react-table";
+import MOCK_DATA from "./MOCK_DATA.json";
+import "./style.css";
+export const BasicTable = () => {
+  const columns = useMemo(() => COLUMNS, []);
+  const data = useMemo(() => MOCK_DATA, []);
+  const tableInstance = useTable(
+    {
+      columns,
+      data,
+    },
+    useSortBy
+  );
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    tableInstance;
+  console.log(headerGroups);
+  return (
+    <>
+      <table {...getTableProps()}>
+        <thead>
+          {" "}
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                  {column.render("Header")}
+                  <span>
+                    {column.isSorted ? (column.isSortedDesc ? ">" : "^") : ""}{" "}
+                  </span>
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
+};
